ad619b93493b9dda5c1f43c735f9b9a7426389d3
alejandriadavila <149810746+alejandriadavila@users.noreply.github.com>
2023-11-29 22:08:38 -0500

    prime numbers

--- loops.js
+++ loops.js
@@ -1,14 +1,25 @@
-let x = 1
+// let x = 1
 
-while (x <= 100){
-    if(x % 3 === 0 && x % 5 === 0){
-        console.log(`Fizz Buzz`)
-    }else if(x % 3 === 0){
-        console.log(`Fizz`)
-    }else if(x % 5 ===0){
-        console.log(`Buzz`)
+// while (x <= 100){
+//     if(x % 3 === 0 && x % 5 === 0){
+//         console.log(`Fizz Buzz`)
+//     }else if(x % 3 === 0){
+//         console.log(`Fizz`)
+//     }else if(x % 5 ===0){
+//         console.log(`Buzz`)
+//     }else{
+//         console.log(x)
+//     }
+//     x ++
+// }
+
+let n = 4
+
+for(let i = 2; i < n; i++){
+    if(n % i !== 0){
+        console.log(n)
+        break;
     }else{
-        console.log(x)
+        n++
     }
-    x ++
 }
