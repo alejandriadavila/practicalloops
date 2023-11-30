19c880ff9141065c217ea830589fb86ad67f6e37
alejandriadavila <149810746+alejandriadavila@users.noreply.github.com>
2023-11-30 16:02:56 -0500

    Part 1 & 2 Finished

--- loops.js
+++ loops.js
@@ -1,19 +1,19 @@
-// let x = 1
+let x = 1
 
-// while (x <= 100){
-//     if(x % 3 === 0 && x % 5 === 0){
-//         console.log(`Fizz Buzz`)
-//     }else if(x % 3 === 0){
-//         console.log(`Fizz`)
-//     }else if(x % 5 ===0){
-//         console.log(`Buzz`)
-//     }else{
-//         console.log(x)
-//     }
-//     x ++
-// }
+while (x <= 100){
+    if(x % 3 === 0 && x % 5 === 0){
+        console.log(`Fizz Buzz`)
+    }else if(x % 3 === 0){
+        console.log(`Fizz`)
+    }else if(x % 5 ===0){
+        console.log(`Buzz`)
+    }else{
+        console.log(x)
+    }
+    x ++
+}
 
-let n = 4
+let n = 5
 
 for(let i = 2; i < n; i++){
     if(n % i !== 0){
@@ -23,3 +23,5 @@ for(let i = 2; i < n; i++){
         n++
     }
 }
+
+let array = [ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26]
